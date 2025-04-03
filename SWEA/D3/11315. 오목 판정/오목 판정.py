6f2273fe227bbf59f2b89b9c2cc59454220abf1b
Xiumin1714 <ksm1714@snu.ac.kr>
2025-04-03 17:27:18 +0900

    [D3] Title: 오목 판정, Time: 118 ms, Memory: 60,416 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,28 +1,27 @@
-def dfs(arr, N):
+def Omok(N):
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 'o':
                 cnt = 1
-                for di, dj in [[1,0],[1,-1],[0,1],[0,-1],[1,1],[-1,-1],[-1,0],[-1,1]]:
+                for di, dj in [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]:
                     for c in range(1, 5):
-                        ni, nj = i+di*c, j+dj*c
-                        if 0<=ni<N and 0<=nj<N and arr[ni][nj] != '.':
+                        ni, nj = i + di * c, j + dj * c
+                        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 'o':
                             cnt += 1
                             if cnt == 5:
                                 return 'YES'
                         else:
                             cnt = 1
-                            break       # for c
+                            break
     return 'NO'
 
 
 T = int(input())
-
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(input()) for _ in range(N)]
+
     # print(arr)
+    ans = Omok(N)
 
-    result = dfs(arr, N)
-
-    print(f'#{tc} {result}')
+    print(f'#{tc} {ans}')
