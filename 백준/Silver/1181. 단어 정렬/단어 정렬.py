50e6640c8add583b27a9cb9c6f1714a124a6f839
Xiumin1714 <ksm1714@snu.ac.kr>
2025-04-08 22:13:58 +0900

    [Silver V] Title: 단어 정렬, Time: 564 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -2,13 +2,9 @@ N = int(input())
 arr = [input() for _ in range(N)]
 # print(arr)
 
-arr.sort()
-arr.sort(key = len)
+ans = list(set(arr))    # 중복 제거
+ans.sort()      # 알파벳 순서 정렬
+ans.sort(key=len)   # 문자열 길이 기준 정렬
 
-ans = []
-for i in arr:
-    if i not in ans:
-        ans.append(i)
-
-for j in ans:
-    print(j)
+for i in ans:
+    print(i)
