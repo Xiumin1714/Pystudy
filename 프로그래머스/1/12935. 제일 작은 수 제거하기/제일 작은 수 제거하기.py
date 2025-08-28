4aa8f59a25f8bed269b237f32fad46201adc5dd1
Xiumin1714 <ksm1714@snu.ac.kr>
2025-08-28 15:04:51 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.08 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,12 +1,5 @@
 def solution(arr):
-    answer = []
-    min_val = 1000000
-    
-    for i in range(len(arr)):
-        if min_val > arr[i]:
-            min_val = arr[i]
-    
-    arr.remove(min_val)
+    arr.remove(min(arr))
     if len(arr) == 0:
         arr.append(-1)
 
